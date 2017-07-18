02ee9cdcb24fba4c838dc2e01af00b31b647f4b3
Mauri Edo <medo@atlassian.com>
2017-07-19 09:41:42 +1000

    revert: release 0.2.0
    This reverts commit dd3251b624f2e59b7e735cd31b574c4a6b330f42.

--- dist/openapi-diff/result-reporter.js
+++ dist/openapi-diff/result-reporter.js
@@ -1,27 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
-exports.default = {
-    build: (result) => {
-        const changeList = [];
-        const summary = [];
-        if (_.isEmpty(result.breakingChanges)) {
-            summary.push('0 breaking changes found.');
-        }
-        summary.push(`${result.nonBreakingChanges.length} non-breaking changes found.`);
+const processResult = (result) => {
+    let processedResult;
+    const changeList = [];
+    const summary = [];
+    if (_.isEmpty(result.breakingChanges)) {
+        summary.push('0 breaking changes found.');
+    }
+    if (_.isEmpty(result.nonBreakingChanges)) {
+        summary.push('0 non-breaking changes found.');
+    }
+    else {
+        let nonBreakingChangeCount = 0;
         for (const entry of result.nonBreakingChanges) {
+            nonBreakingChangeCount += 1;
             changeList.push(`Non-breaking: the path [${entry.path.join('/')}] `
                 + `was modified from \'${entry.lhs}\' to \'${entry.rhs}\'`);
         }
-        summary.push(`${result.unclassifiedChanges.length} unclassified changes found.`);
+        summary.push(`${nonBreakingChangeCount} non-breaking changes found.`);
+    }
+    if (_.isEmpty(result.unclassifiedChanges)) {
+        summary.push('0 unclassified changes found.');
+    }
+    else {
+        let unclassifiedChangeCount = 0;
         for (const entry of result.unclassifiedChanges) {
+            unclassifiedChangeCount += 1;
             changeList.push(`Unclassified: the path [${entry.path.join('/')}] `
                 + `was modified from \'${entry.lhs}\' to \'${entry.rhs}\'`);
         }
-        const processedResult = {
-            changeList,
-            summary
-        };
-        return processedResult;
+        summary.push(`${unclassifiedChangeCount} unclassified changes found.`);
+    }
+    processedResult = {
+        changeList,
+        summary
+    };
+    return processedResult;
+};
+exports.default = {
+    print: (result) => {
+        return processResult(result);
     }
 };
