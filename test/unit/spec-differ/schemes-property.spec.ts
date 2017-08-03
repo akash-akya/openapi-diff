5c05ccf2183c4956cf45df959dc6934d5d4a8e66
Mauri Edo <medo@atlassian.com>
2017-08-03 15:22:26 +1000

    test: add missing kind assertion to differ tests

--- test/unit/spec-differ/schemes-property.spec.ts
+++ test/unit/spec-differ/schemes-property.spec.ts
@@ -5,7 +5,7 @@ import {
     ParsedSpec
 } from '../../../lib/openapi-diff/types';
 
-let result: DiffChange[];
+let results: DiffChange[];
 
 describe('specDiffer', () => {
 
@@ -29,36 +29,36 @@ describe('specDiffer', () => {
             oldParsedSpec.schemes = ['schema'];
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
             newParsedSpec.schemes = ['secure schema'];
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should classify the edition of an element in the schemes property content as breaking', () => {
-            expect(result.length).toEqual(1);
-            expect(result[0].severity).toEqual('breaking');
+            expect(results.length).toEqual(1);
+            expect(results[0].severity).toEqual('breaking');
         });
 
         it('should locate the scope of the change in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
         });
 
         it('should populate the taxonomy and type of a new schemes property as an edition', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.edit');
-            expect(result[0].type).toEqual('edit');
+            expect(results[0].taxonomy).toEqual('schemes.property.edit');
+            expect(results[0].type).toEqual('edit');
         });
 
         it('should populate the paths of an added schemes property correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].path[1].toString()).toEqual('0');
-            expect(result[0].printablePath[0]).toEqual('schemes');
-            expect(result[0].printablePath[1].toString()).toEqual('0');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].path[1].toString()).toEqual('0');
+            expect(results[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].printablePath[1].toString()).toEqual('0');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toBe('schema');
-            expect(result[0].rhs).toEqual('secure schema');
-            expect(result[0].index).toBeNull();
-            expect(result[0].item).toBeNull();
-            expect(result[0].kind).toEqual('E');
+            expect(results[0].lhs).toBe('schema');
+            expect(results[0].rhs).toEqual('secure schema');
+            expect(results[0].index).toBeNull();
+            expect(results[0].item).toBeNull();
+            expect(results[0].kind).toEqual('E');
         });
     });
 
@@ -68,35 +68,35 @@ describe('specDiffer', () => {
             oldParsedSpec.schemes = [];
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
             newParsedSpec.schemes = ['schema'];
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should classify the addition of an element in the schemes property content as non-breaking', () => {
-            expect(result.length).toEqual(1);
-            expect(result[0].severity).toEqual('non-breaking');
+            expect(results.length).toEqual(1);
+            expect(results[0].severity).toEqual('non-breaking');
         });
 
         it('should locate the scope of the change in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
         });
 
         it('should populate the taxonomy and type of a new schemes property as an addition', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.arrayContent.add');
-            expect(result[0].type).toEqual('arrayContent.add');
+            expect(results[0].taxonomy).toEqual('schemes.property.arrayContent.add');
+            expect(results[0].type).toEqual('arrayContent.add');
         });
 
         it('should populate the paths of an added schemes property correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].printablePath[0]).toEqual('schemes');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toBeNull();
-            expect(result[0].rhs).toEqual('schema');
-            expect(result[0].index).toEqual(0);
-            expect(result[0].item).toEqual(jasmine.objectContaining({ kind: 'N' }));
-            expect(result[0].item).toEqual(jasmine.objectContaining({ rhs: 'schema' }));
-            expect(result[0].kind).toEqual('A');
+            expect(results[0].lhs).toBeNull();
+            expect(results[0].rhs).toEqual('schema');
+            expect(results[0].index).toEqual(0);
+            expect(results[0].item).toEqual(jasmine.objectContaining({ kind: 'N' }));
+            expect(results[0].item).toEqual(jasmine.objectContaining({ rhs: 'schema' }));
+            expect(results[0].kind).toEqual('A');
         });
     });
 
@@ -106,35 +106,35 @@ describe('specDiffer', () => {
             oldParsedSpec.schemes = ['schema'];
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
             newParsedSpec.schemes = [];
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should classify the deletion of an element in the schemes property content as breaking', () => {
-            expect(result.length).toEqual(1);
-            expect(result[0].severity).toEqual('breaking');
+            expect(results.length).toEqual(1);
+            expect(results[0].severity).toEqual('breaking');
         });
 
         it('should locate the scope of the change in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
         });
 
         it('should populate the taxonomy and type of a new schemes property as a deletion', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.arrayContent.delete');
-            expect(result[0].type).toEqual('arrayContent.delete');
+            expect(results[0].taxonomy).toEqual('schemes.property.arrayContent.delete');
+            expect(results[0].type).toEqual('arrayContent.delete');
         });
 
         it('should populate the paths of an added schemes property correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].printablePath[0]).toEqual('schemes');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toEqual('schema');
-            expect(result[0].rhs).toBeNull();
-            expect(result[0].index).toEqual(0);
-            expect(result[0].item).toEqual(jasmine.objectContaining({ kind: 'D' }));
-            expect(result[0].item).toEqual(jasmine.objectContaining({ lhs: 'schema' }));
-            expect(result[0].kind).toEqual('A');
+            expect(results[0].lhs).toEqual('schema');
+            expect(results[0].rhs).toBeNull();
+            expect(results[0].index).toEqual(0);
+            expect(results[0].item).toEqual(jasmine.objectContaining({ kind: 'D' }));
+            expect(results[0].item).toEqual(jasmine.objectContaining({ lhs: 'schema' }));
+            expect(results[0].kind).toEqual('A');
         });
     });
 
@@ -143,34 +143,34 @@ describe('specDiffer', () => {
             const oldParsedSpec = buildParsedSpecWithoutSchemesProperty();
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
             newParsedSpec.schemes = ['NEW schema'];
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should classify the addition of the schemes property as breaking', () => {
-            expect(result.length).toEqual(1);
-            expect(result[0].severity).toEqual('breaking');
+            expect(results.length).toEqual(1);
+            expect(results[0].severity).toEqual('breaking');
         });
 
         it('should locate the scope of the change in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
         });
 
         it('should populate the taxonomy and type of a new schemes property as an addition', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.add');
-            expect(result[0].type).toEqual('add');
+            expect(results[0].taxonomy).toEqual('schemes.property.add');
+            expect(results[0].type).toEqual('add');
         });
 
         it('should populate the paths of an added schemes property correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].printablePath[0]).toEqual('schemes');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toBeNull();
-            expect(result[0].rhs).toEqual(['NEW schema']);
-            expect(result[0].index).toBeNull();
-            expect(result[0].item).toBeNull();
-            expect(result[0].kind).toEqual('N');
+            expect(results[0].lhs).toBeNull();
+            expect(results[0].rhs).toEqual(['NEW schema']);
+            expect(results[0].index).toBeNull();
+            expect(results[0].item).toBeNull();
+            expect(results[0].kind).toEqual('N');
         });
     });
 
@@ -179,34 +179,34 @@ describe('specDiffer', () => {
             const oldParsedSpec = buildParsedSpecWithoutSchemesProperty();
             oldParsedSpec.schemes = ['schema'];
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should classify the removal of the schemes property as breaking', () => {
-            expect(result.length).toEqual(1);
-            expect(result[0].severity).toEqual('breaking');
+            expect(results.length).toEqual(1);
+            expect(results[0].severity).toEqual('breaking');
         });
 
         it('should locate the scope of the change in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
         });
 
         it('should populate the taxonomy and type of a new schemes property as an deletion', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.delete');
-            expect(result[0].type).toEqual('delete');
+            expect(results[0].taxonomy).toEqual('schemes.property.delete');
+            expect(results[0].type).toEqual('delete');
         });
 
         it('should populate the paths of a deleted schemes property correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].printablePath[0]).toEqual('schemes');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toEqual(['schema']);
-            expect(result[0].rhs).toBeNull();
-            expect(result[0].index).toBeNull();
-            expect(result[0].item).toBeNull();
-            expect(result[0].kind).toEqual('D');
+            expect(results[0].lhs).toEqual(['schema']);
+            expect(results[0].rhs).toBeNull();
+            expect(results[0].index).toBeNull();
+            expect(results[0].item).toBeNull();
+            expect(results[0].kind).toEqual('D');
         });
     });
 
@@ -216,88 +216,88 @@ describe('specDiffer', () => {
             oldParsedSpec.schemes = ['schema one', 'schema two', 'schema three'];
             const newParsedSpec = buildParsedSpecWithoutSchemesProperty();
             newParsedSpec.schemes = ['schema one', 'three', 'schema four', 'schema five'];
-            result = specDiffer.diff(oldParsedSpec, newParsedSpec);
+            results = specDiffer.diff(oldParsedSpec, newParsedSpec);
         });
 
         it('should detect the right amount of changes', () => {
-            expect(result.length).toEqual(3);
+            expect(results.length).toEqual(3);
         });
 
         it('should classify the first change (edition of schemes member) as breaking', () => {
-            expect(result[0].severity).toEqual('breaking');
+            expect(results[0].severity).toEqual('breaking');
         });
 
         it('should populate the taxonomy and type of the change (edition of schemes member) as an edition', () => {
-            expect(result[0].taxonomy).toEqual('schemes.property.edit');
-            expect(result[0].type).toEqual('edit');
+            expect(results[0].taxonomy).toEqual('schemes.property.edit');
+            expect(results[0].type).toEqual('edit');
         });
 
         it('should populate the paths of the change (edition of schemes member) correctly', () => {
-            expect(result[0].path[0]).toEqual('schemes');
-            expect(result[0].path[1].toString()).toEqual('1');
-            expect(result[0].printablePath[0]).toEqual('schemes');
-            expect(result[0].printablePath[1].toString()).toEqual('1');
+            expect(results[0].path[0]).toEqual('schemes');
+            expect(results[0].path[1].toString()).toEqual('1');
+            expect(results[0].printablePath[0]).toEqual('schemes');
+            expect(results[0].printablePath[1].toString()).toEqual('1');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[0].lhs).toBe('schema two');
-            expect(result[0].rhs).toEqual('three');
-            expect(result[0].index).toBeNull();
-            expect(result[0].item).toBeNull();
-            expect(result[0].kind).toEqual('E');
+            expect(results[0].lhs).toBe('schema two');
+            expect(results[0].rhs).toEqual('three');
+            expect(results[0].index).toBeNull();
+            expect(results[0].item).toBeNull();
+            expect(results[0].kind).toEqual('E');
         });
 
         it('should classify the second change (edition of schemes member) as breaking', () => {
-            expect(result[1].severity).toEqual('breaking');
+            expect(results[1].severity).toEqual('breaking');
         });
 
         it('should populate the taxonomy and type of the change (edition of schemes member) as an edition', () => {
-            expect(result[1].taxonomy).toEqual('schemes.property.edit');
-            expect(result[1].type).toEqual('edit');
+            expect(results[1].taxonomy).toEqual('schemes.property.edit');
+            expect(results[1].type).toEqual('edit');
         });
 
         it('should populate the paths of the change (edition of schemes member) correctly', () => {
-            expect(result[1].path[0]).toEqual('schemes');
-            expect(result[1].path[1].toString()).toEqual('2');
-            expect(result[1].printablePath[0]).toEqual('schemes');
-            expect(result[1].printablePath[1].toString()).toEqual('2');
+            expect(results[1].path[0]).toEqual('schemes');
+            expect(results[1].path[1].toString()).toEqual('2');
+            expect(results[1].printablePath[0]).toEqual('schemes');
+            expect(results[1].printablePath[1].toString()).toEqual('2');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[1].lhs).toBe('schema three');
-            expect(result[1].rhs).toEqual('schema four');
-            expect(result[1].index).toBeNull();
-            expect(result[1].item).toBeNull();
-            expect(result[1].kind).toEqual('E');
+            expect(results[1].lhs).toBe('schema three');
+            expect(results[1].rhs).toEqual('schema four');
+            expect(results[1].index).toBeNull();
+            expect(results[1].item).toBeNull();
+            expect(results[1].kind).toEqual('E');
         });
 
         it('should classify the third change (new schemes member) as non-breaking', () => {
-            expect(result[2].severity).toEqual('non-breaking');
+            expect(results[2].severity).toEqual('non-breaking');
         });
 
         it('should populate the taxonomy and type of the change (new schemes member) as an addition', () => {
-            expect(result[2].taxonomy).toEqual('schemes.property.arrayContent.add');
-            expect(result[2].type).toEqual('arrayContent.add');
+            expect(results[2].taxonomy).toEqual('schemes.property.arrayContent.add');
+            expect(results[2].type).toEqual('arrayContent.add');
         });
 
         it('should populate the paths of the change (new schemes member) correctly', () => {
-            expect(result[2].path[0]).toEqual('schemes');
-            expect(result[2].printablePath[0]).toEqual('schemes');
+            expect(results[2].path[0]).toEqual('schemes');
+            expect(results[2].printablePath[0]).toEqual('schemes');
         });
 
         it('should copy the rest of the individual diff attributes across', () => {
-            expect(result[2].lhs).toBeNull();
-            expect(result[2].rhs).toEqual('schema five');
-            expect(result[2].index).toEqual(3);
-            expect(result[2].item).toEqual(jasmine.objectContaining({ kind: 'N' }));
-            expect(result[2].item).toEqual(jasmine.objectContaining({ rhs: 'schema five' }));
-            expect(result[2].kind).toEqual('A');
+            expect(results[2].lhs).toBeNull();
+            expect(results[2].rhs).toEqual('schema five');
+            expect(results[2].index).toEqual(3);
+            expect(results[2].item).toEqual(jasmine.objectContaining({ kind: 'N' }));
+            expect(results[2].item).toEqual(jasmine.objectContaining({ rhs: 'schema five' }));
+            expect(results[2].kind).toEqual('A');
         });
 
         it('should locate the scope of all the changes in the schemes property', () => {
-            expect(result[0].scope).toEqual('schemes.property');
-            expect(result[1].scope).toEqual('schemes.property');
-            expect(result[2].scope).toEqual('schemes.property');
+            expect(results[0].scope).toEqual('schemes.property');
+            expect(results[1].scope).toEqual('schemes.property');
+            expect(results[2].scope).toEqual('schemes.property');
         });
     });
 });
