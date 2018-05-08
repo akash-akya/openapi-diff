2d86589b8b35fcdfe1913174d94caa992324b1b5
Igor Sechyn <isechyn@atlassian.com>
2018-05-08 15:50:19 +1000

    chore: removed node 4.x support

--- bin/build.sh
+++ bin/build.sh
@@ -2,12 +2,6 @@ echo ==== Load nvm ====
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
-echo ==== Test using node 4.x ====
-nvm install 4.8.7
-rm -rf node_modules
-npm install
-npm test
-
 echo ==== Test using node 6.x ====
 nvm install 6.12.3
 rm -rf node_modules
